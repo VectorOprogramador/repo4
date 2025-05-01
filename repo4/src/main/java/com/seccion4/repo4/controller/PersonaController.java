17a53519d23e83207178367c5974bc36b8c4216f
Diego Améstica <di.amestica@duocuc.cl>
2025-05-01 18:07:08 -0400

    Controller listo

---
+++ repo4/src/main/java/com/seccion4/repo4/controller/PersonaController.java
@@ -0,0 +1,17 @@
+package com.seccion4.repo4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/api/personas")
+@RestController
+public class PersonaController {
+    
+    @GetMapping
+    public String Saludar()
+    {
+        return "Hola esto esta funcionando !!";
+    }
+}
